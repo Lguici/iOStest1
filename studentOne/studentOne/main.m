f097bf4e0471bae4333605e57176cf02bf486af0
guici <121721787@qq.com>
2024-05-26 18:33:50 +0800

    Add File

---
+++ studentOne/studentOne/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  studentOne
+//
+//  Created by 刘原甫 on 2024/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
